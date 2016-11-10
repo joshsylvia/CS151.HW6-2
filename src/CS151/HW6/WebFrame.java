3b4ecea2aaddbf49ce4fe54be604ef4d009c662a
joshsylvia <User@10.0.0.17>
2016-11-09 17:17:25 -0800

    initial commit

---
+++ src/CS151/HW6/WebFrame.java
@@ -0,0 +1,10 @@
+package CS151.HW6;
+
+public class WebFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/CS151/HW6/WebWorker.java
@@ -0,0 +1,10 @@
+package CS151.HW6;
+
+public class WebWorker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
